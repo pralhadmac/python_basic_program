2d96a9340cd6e7cc00ce657702f85af0f0dbbe01
Pralhad.Gotarne <pagotarne@gmail.com>
2026-01-20 18:06:13 +0530

    Pattern Program added

---
+++ PatternProgram.py
@@ -0,0 +1,22 @@
+print(" Inverted pattern ")
+n = 5
+
+for i in range(n,0,-1):
+    print("*" * i)
+
+print("Right Angle Pattern")
+n = 5
+for i in range(0,n,+1):
+    print("*" * i)
+
+print(" Square Star Pattern")
+n = 5
+for i in range(n):
+    print("*" * n)
+
+print("Pyramid Pattern")
+n = 5
+for i in range(n):
+    print(" " *(n - i - 1) + "*" *(2*i+1))
+
+print("Inverted pyramid pattern")
