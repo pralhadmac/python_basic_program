30e2821704bcf2081672dcba824d7e8b89157990
Pralhad.Gotarne <pagotarne@gmail.com>
2026-01-22 10:33:10 +0530

    Inverted pyramid code updated

--- PatternProgram.py
+++ PatternProgram.py
@@ -20,3 +20,12 @@ for i in range(n):
     print(" " *(n - i - 1) + "*" *(2*i+1))
 
 print("Inverted pyramid pattern")
+n= 5
+for i in range(n,0,-1):
+    print(" "*(n-i)+"* " *i)
+
+print("Inverted pyramid pattern with full fill")
+
+n = 5
+for i in range(n, 0, -1):
+    print(" " * (n - i) + "*" * (2*i - 1))
